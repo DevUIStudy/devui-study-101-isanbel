e9eea71c658e3f6e866f920fd5de23bf0882b2a1
isanbel <theisanbel@gmail.com>
2019-11-03 17:05:25 +0800

    Add init files page and start using d-accordion

--- src/app/admin-files/admin-files.component.ts
+++ src/app/admin-files/admin-files.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-files.component.css']
 })
 export class AdminFilesComponent implements OnInit {
-
+  menu = [{
+    title: '通用设置',
+    children: [
+      {title: '子内容1'},
+      {title: '子内容2'},
+      {title: '子内容3'},
+    ]
+  }, {
+    title: '项目管理',
+    children: [
+      {title: '项目设置模版'},
+      {title: '子内容2'},
+      {title: '子内容3'},
+    ]
+  }, {
+    title: '内容三（默认展开）',
+    open: true,
+    children: [
+      {title: '子内容1(禁用)', disabled: true},
+      {title: '子内容2(默认激活)', active: true},
+      {title: '子内容3'},
+    ]
+  }, {
+    title: '内容四（没有子项）' 
+  }];
   constructor() { }
 
   ngOnInit() {
