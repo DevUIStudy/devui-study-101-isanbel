c4de1391d558aa025545689c5986a416fec8e24e
isanbel <theisanbel@gmail.com>
2019-11-07 15:17:25 +0800

    Add: new chart component

---
+++ src/app/admin-files-chart/admin-files-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-files-chart',
+  templateUrl: './admin-files-chart.component.html',
+  styleUrls: ['./admin-files-chart.component.scss']
+})
+export class AdminFilesChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AdminFilesComponent } from './admin-files/admin-files.component';
+import { AdminFilesChartComponent } from './admin-files-chart/admin-files-chart.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       PageNotFoundComponent,
-      AdminFilesComponent
+      AdminFilesComponent,
+      AdminFilesChartComponent
    ],
    imports: [
       BrowserModule,
