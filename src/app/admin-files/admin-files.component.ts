d8d509c40a7101dbd51db82b970c0774526c373d
isanbel <theisanbel@gmail.com>
2019-11-07 14:50:28 +0800

    Update: left menu, postion and default active settings

--- src/app/admin-files/admin-files.component.ts
+++ src/app/admin-files/admin-files.component.ts
@@ -17,21 +17,21 @@ export class AdminFilesComponent implements OnInit {
     ]
   }, {
     title: '项目管理',
+    open: true,
     children: [
       {title: '项目设置模版'},
+      {title: '设置项目创建者'},
+      {title: '项目和成员管理', active: true},
+    ]
+  }, {
+    title: '变异架构',
+    children: [
+      {title: '子内容1(禁用)', disabled: true},
       {title: '子内容2'},
       {title: '子内容3'},
     ]
   }, {
-    title: '内容三（默认展开）',
-    open: true,
-    children: [
-      {title: '子内容1(禁用)', disabled: true},
-      {title: '子内容2(默认激活)', active: true},
-      {title: '子内容3'},
-    ]
-  }, {
-    title: '内容四（没有子项）' 
+    title: '私有一来哭' 
   }];
   constructor() { }
 
